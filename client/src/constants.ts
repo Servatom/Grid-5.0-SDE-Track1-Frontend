dba952f4801b7c1a2ab2102044483942bb16c78e
Yashvardhan Arora <yash22arora@gmail.com>
2023-08-21 03:35:37 +0530

    Fix type error for avg rating
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -11,7 +11,7 @@ export const ORDERS: IOrder[] = [
         color: "blue",
         image:
           "https://apisap.fabindia.com/medias/10658518-1.jpg?context=bWFzdGVyfGltYWdlc3wyNDgwNDN8aW1hZ2UvanBlZ3xoOGEvaDgxLzg4MTEyMjMzODQwOTQvMTA2NTg1MThfMS5qcGd8ZWQ0OWViNDAzOTQ4YWZiYmFiNGUzNDgwNjAwY2VjYjY0Yjg0OGRhOThhMDIxMDgzYzdhMDQ0MzIxMDA0NmEzMA",
-        avgRating: 4.3,
+        average_rating: 4.3,
         description:
           "Get the spotlight on you where ever you go with this contemporary and stylish long kurta from FabIndia. It is an ethnic wardrobe staple and can be worn to any and every occasion when paired correctly with a churidar or pyjama and ethnic jutties. Crafted from cotton fabric with a geometric pattern, this kurta is topped with a Chinese collar and framed with full sleeves cut in a slim fit silhouette, highlighted with a contrast button on the front placket.",
         size: "M",
@@ -23,7 +23,7 @@ export const ORDERS: IOrder[] = [
         color: "grey",
         image:
           "https://apisap.fabindia.com/medias/10700598-2.jpg?context=bWFzdGVyfGltYWdlc3wzODg2MTB8aW1hZ2UvanBlZ3xoNTcvaDQzLzg5MjAwMjAzNTMwNTQvMTA3MDA1OThfMi5qcGd8NzFhMTE1ODI5OTZkYjU5NzFkZDI2Yjg3ODVmYmIwMjcwOTg4YzgzZTU5NzI1M2Q0MDE2ZjYwZGVkOGQzNjNiNg",
-        avgRating: 3.9,
+        average_rating: 3.9,
         description:
           "A splendid pick for a day out or an evening planned with family, this long kurta from FabIndia is just perfect for you. This alluring hand block print and an attractive colour palette coalesce to create the perfect outfit you can wear to any casual event at any time when paired correctly with a churidar or pyjama and ethnic jutties. Made from breathable Cotton fabric, this kurta is topped with a Chinese collar and full sleeves.",
         size: "M",
@@ -40,7 +40,7 @@ export const ORDERS: IOrder[] = [
         color: "grey",
         image:
           "https://images.bestsellerclothing.in/data/JJ/aug-30-2022/186543901_g1.jpg?width=488&height=650&mode=fill&fill=blur&format=auto",
-        avgRating: 4.2,
+        average_rating: 4.2,
         description:
           "Look cool wearing this light grey jacket from Jack&Jones. Features a high-neck, front zip closure, and front pockets. Pair it with our matching pants for a cool look!",
         size: "M",
